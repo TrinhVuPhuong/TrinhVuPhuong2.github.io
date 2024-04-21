5d87bef99ad56f271dba1efa03c2e7b3b55e9632
TrinhVuPhuong <sipnhocprooo@gmail.com>
2024-04-21 15:23:18 +0700

    Update sheets.js

--- sheets.js
+++ sheets.js
@@ -105,14 +105,41 @@ function maker() {
         p.textContent ="Số lượng: " + obj['quantity']+" Hộp";
         div.append(p);
 
+        p= document.createElement('p');
+        p.setAttribute("class","tittle")
+
+        const api ='https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + obj['iddt']+ "_" + obj['idtua'] + "_" + obj['idkh'] + "_" + titleCase(decodeURIComponent(obj['customername'])) + "_" + obj['quantity']
+
+                var img = document.createElement('img');
+
+                // Thiết lập thuộc tính src
+                img.setAttribute('src', api);
+
+                // Thiết lập thuộc tính alt
+                img.setAttribute('alt', 'qrimage');
+
+                // Thiết lập thuộc tính id
+                img.setAttribute('id', 'qrimage');
+
+                p.appendChild(img)
+
+        div.appendChild(p);
+
 
 
     output.append(div);
 
-    
- 
+    var sttValue = obj['stt']; // Giá trị của obj['stt']
+
+
+
+
 }
 maker();
+
+
+
+
 
 window.print();
 if(obj['thietbi']=='computer'){
